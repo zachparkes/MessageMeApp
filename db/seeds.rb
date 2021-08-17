f6c4adaa7a6a0b88e3248dcc682f63da1ab71a93
Zach Parkes <zach@sprigghr.com>
2021-08-17 14:23:47 -0400

    display actual messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Zach", password: "password")
+User.create(username: "Brian", password: "password")
+User.create(username: "Parkes", password: "password")
+User.create(username: "ZBP", password: "password")
+User.create(username: "Zachary", password: "password")
+
+Message.create(body: "Message from seeds", user: User.first)
