f8043fbfb585cddd5c6d6f23b3ede6228d91171d
Zach Parkes <zach@sprigghr.com>
2021-08-24 15:06:43 -0400

    Generate chatroom channel and add route, add method to broadcast messages, display messages realtime

---
+++ app/channels/chatroom_channel.rb
@@ -0,0 +1,11 @@
+class ChatroomChannel < ApplicationCable::Channel
+
+  def subscribed
+    stream_from "chatroom_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+end
